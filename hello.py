cf77f0da1c58390500d910edab0616b25ed879c5
YutaroUeno0108 <yutaroueno@uenoyuutarou-no-MacBook-Air-2.local>
2017-02-28 08:40:36 +0900

    Flask

---
+++ hello.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # 「templates/index.html」のテンプレートを使う
+    # 「message」という変数に"Hello"と代入した状態で、テンプレート内で使う
+    return render_template('index.html', message="Hello")
+
+if __name__ == "__main__":
+    app.run()
+    
